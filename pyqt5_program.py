b4d1c1ac962ae5102b903ee23ca2893b1f011806
shamin daniil <danya.shamin.06@mail.ru>
2022-08-26 10:04:00 +0300

    add one window

--- pyqt5_program.py
+++ pyqt5_program.py
@@ -1,11 +1,16 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 
+f_program_name = sys.argv[0]
+f_program = open(f_program_name, 'br')
+print(f_program.read()[0])
 if __name__ == '__main__':
+    print(sys.argv)
     app = QApplication(sys.argv)
-    widget_1 = QWidget()
-    widget_1.resize(500, 300)
-    widget_1.setWindowTitle('Открылось окно, закрой')
-    widget_1.move(300, 300)
-    widget_1.show()
+    widget = QWidget()
+    widget.resize(250, 150)
+    widget.move(100, 100)
+    widget.setWindowTitle('Window')
+    widget.show()
     sys.exit(app.exec_())
+    
