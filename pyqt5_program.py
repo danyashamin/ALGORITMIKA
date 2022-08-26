b8d76f552dc60f014087772bf17af2180fc42a12
shamin daniil <danya.shamin.06@mail.ru>
2022-08-26 11:24:51 +0300

    start _pyqt5

--- pyqt5_program.py
+++ pyqt5_program.py
@@ -2,15 +2,16 @@ from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 
 f_program_name = sys.argv[0]
-f_program = open(f_program_name, 'br')
-print(f_program.read()[0])
+file_program = open(f_program_name, 'br')
+print(file_program.read()[0])
+
 if __name__ == '__main__':
     print(sys.argv)
     app = QApplication(sys.argv)
     widget = QWidget()
-    widget.resize(250, 150)
-    widget.move(100, 100)
-    widget.setWindowTitle('Window')
+    widget.resize(800, 600)
+    widget.move(150, 150)
+    widget.setWindowTitle('Title')
+    widget.setObjectName('form')
     widget.show()
     sys.exit(app.exec_())
-    
