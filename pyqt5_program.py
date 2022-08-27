3ef01ee39765ba7cfe18d8ef9d6679109d3a51cc
shamin daniil <danya.shamin.06@mail.ru>
2022-08-27 11:35:32 +0300

    add widget and it Title

--- pyqt5_program.py
+++ pyqt5_program.py
@@ -1,11 +1,13 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QApplication
-import sys
-
-if __name__ == '__main__':
-    application_1 = QApplication(sys.argv)
-    widget_1 = QWidget()
-    widget_1.resize(500, 400)
-    widget_1.move(300, 300)
-    widget_1.setWindowTitle('My_title')
-    widget_1.show()
-    sys.argv(application_1.exec_())
+def record(func):
+    def cycleRecotd():
+        func()
+    return cycleRecotd
+@record
+def funcArgOne():
+    print(1)
+def funcArgTwo():
+    print(2)
+funObj = record(funcArgOne)
+funObj()
+funcObjSecond = record(funcArgTwo)
+funcObjSecond()
