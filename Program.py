3e6c8b4c81e8248d52902c47188f81939ab8a556
shamin daniil <danya.shamin.06@mail.ru>
2022-08-31 20:35:14 +0300

    try to use magic functions in program

--- Program.py
+++ Program.py
@@ -1,17 +1,10 @@
-from msilib.schema import Condition
+class SprotMen():
+    def __init__(self, awards):
+        self.awards = awards
+    def __gt__(self, otherSportMen):
+        return len(self.awards)>len(otherSportMen.awards)
 
-
-class ProgramObj():
-    def __init__(self, ObjName):
-        self.file = open(ObjName)
-    def __eq__(self, otherObj):
-        if self.file.name == otherObj.file.name:
-            print('Они равны.')
-    def __del__(self):
-        print(self.file.closed)
-        self.file.close()
-        print(self.file.closed)
-
-obj = ProgramObj('pyqt5_program.py')
-obj_2 = ProgramObj('pyqt5_program.py')
-condition = obj == obj_2
+sprotmenOne = SprotMen(['Кубок', 'Медаль'])
+sportmenTwo = SprotMen(['Грамота за "Русский медвежонок"'])
+condition = sprotmenOne>sportmenTwo
+print(condition)
