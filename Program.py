6ad53dfeb140f4bd784d941a60822370b37d6d08
shamin daniil <danya.shamin.06@mail.ru>
2022-08-31 21:21:52 +0300

    create magic method

--- Program.py
+++ Program.py
@@ -1,10 +1,10 @@
 class ObjectMy():
     def __init__(self, a):
         self.a = a
-    def __eq__(self, other):
+    def __lt__(self, other):
         print('Выполнилось')
         return (self.a, other.a)
 a = ObjectMy('a')
 b = ObjectMy('b')
-c = a==b
+c = a<b
 print(c)
