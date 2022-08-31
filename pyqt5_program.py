b62f9d47dbc60775cab41b51ec86e95a88acd39b
shamin daniil <danya.shamin.06@mail.ru>
2022-08-31 20:44:02 +0300

    add two widgets

--- pyqt5_program.py
+++ pyqt5_program.py
@@ -1,3 +1,4 @@
+from email.mime import application
 from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 
@@ -6,6 +7,12 @@ widgetOne = QWidget()
 widgetOne.resize(500, 300)
 widgetOne.move(300, 300)
 widgetOne.setWindowTitle('MyLovelyWidget')
+applicationTwo = QApplication(sys.argv)
+widgetTwo = QWidget()
+widgetTwo.resize(500, 300)
+widgetTwo.move(800, 300)
+widgetTwo.setWindowTitle('MyNotLovelyWidget')
 if __name__ == '__main__':
     widgetOne.show()
+    widgetTwo.show()
     sys.argv(applicationOne.exec_())
