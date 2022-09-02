cefe13edc014a88bb675f6eac69d6ca0c18b1408
shamin daniil <danya.shamin.06@mail.ru>
2022-09-02 11:50:10 +0300

    add decorator

--- Program.py
+++ Program.py
@@ -1,10 +1,18 @@
-class ObjectMy():
-    def __init__(self, a):
-        self.a = a
-    def __lt__(self, other):
-        print('Выполнилось')
-        return (self.a, other.a)
-a = ObjectMy('a')
-b = ObjectMy('b')
-c = a<b
-print(c)
+def service(func_service):
+    def service_performance():
+        func_service()
+        print('С вас тыщ!')
+    return service_performance
+
+@service
+def hair_cut():
+    print('Я постриг тебе волосы.')
+def shaving():
+    print('Я побрил тебя.')
+
+service_type = input(f'Какую услугу вы желаете?\n')
+qlossery = {'стрижку':hair_cut, 'бритьё':shaving}
+service_obj = service(qlossery[service_type])
+question = input(f'Выполнить?\n')
+if question == 'да':
+    service_obj()
