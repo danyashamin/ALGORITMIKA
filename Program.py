6fbe8501a8ad766ca9df49844f42e44cc2d19ec3
shamin daniil <danya.shamin.06@mail.ru>
2022-09-05 19:56:14 +0300

    use class like decorator

--- Program.py
+++ Program.py
@@ -1,12 +1,8 @@
-from multiprocessing import managers
-
-
-class Manager():
-    def __init__(self, Name, suggestions):
-        self.Name = Name
-        self.suggestions = suggestions
-    def __call__(self):
-        print('My suggestions:', self.suggestions)
-
-managerFirst = Manager('Игорь', ['пойти погулять', 'не пойти погулять'])
-managerFirst()
+class Manage:
+    def __call__(self, suggesting):
+        print('I suggest something...')
+        suggesting()
+def suggesting_1():
+    print('Go for a walk!')
+manage_1 = Manage()
+manage_1(suggesting_1)
