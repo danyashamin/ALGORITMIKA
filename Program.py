99de6f0069134c7b5b3456b48abec455d1ab45e6
shamin daniil <danya.shamin.06@mail.ru>
2022-09-13 18:59:56 +0300

    add magic method and decorator

--- Program.py
+++ Program.py
@@ -1,20 +1,31 @@
-from msilib.schema import Condition
+import pygame as pg
+pg.init()
 
+def management(running, PERFORMANCE_INPUT, inputString):
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == pg.KEYDOWN:
+            inputString+=event.unicode
+            print(event.unicode, inputString)
+    SCREEN.fill(COLORS['RED'])
+    text_cur = font.render(inputString, True, COLORS['WHITE'])
+    pg.draw.rect(SCREEN, COLORS['BLACK'], rect)
+    SCREEN.blit(text_cur, (100, 100))
+    pg.display.update()
+    return running, PERFORMANCE_INPUT, inputString
 
-class Employee():
-    def __init__(self, name, experience):
-        self.name = name
-        self.experience = experience
-    def work(self):
-        print('Выполняю работу. Моя специальность:', self.experience)
-    def __call__(self):
-        self.work()
-        print('И моё имя:', self.name)
-    def __lt__(self, other):
-        print(self, other)
-    def __cmp__(self, other):
-        return 1+other
+clock = pg.time.Clock()
+running = True
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+SCREEN = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+rect = pg.Rect((100, 100, 400, 40))
+PERFORMANCE_INPUT = False
+COLORS = {'WHITE':(255, 255, 255), 'BLACK':(0, 0, 0), 'RED':(255, 0, 0)}
+font = pg.font.Font(None, 100)
+inputString = ''
 
-employee = Employee('Лёша', 'Тракторист')
-employee()
-condition = employee<3
+while running:
+    running, PERFORMANCE_INPUT, inputString = management(running, PERFORMANCE_INPUT, inputString)
+pg.quit()
