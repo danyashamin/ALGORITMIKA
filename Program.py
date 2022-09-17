e3b9fe4ec36426b008b34943b656786b1b1bb653
shamin daniil <danya.shamin.06@mail.ru>
2022-09-17 15:17:58 +0300

    add class Animal with magic method

--- Program.py
+++ Program.py
@@ -1,10 +1,9 @@
 class Animal():
     def __init__(self, typeAnimal, strenght):
         self.typeAnimal = typeAnimal
-        self.type = type
         self.strenght = strenght
     def __add__(self, otherAnimal):
-        print('Привет, ', otherAnimal, ', я - '+self.typeAnimal)
+        print('Привет, ', otherAnimal.typeAnimal, ', я - '+self.typeAnimal)
         print('Давай дружить')
     def __lt__(self, otherAnimal):
         if self.strenght<otherAnimal.strenght:
@@ -15,3 +14,4 @@ class Animal():
 hare = Animal('Заяц', 2)
 wolf = Animal('Волк', 4)
 a = hare<wolf
+b = hare+wolf
