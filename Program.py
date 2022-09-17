4b35ed0bac66d9eb9123caa606d33b04decb081f
shamin daniil <danya.shamin.06@mail.ru>
2022-09-17 15:15:41 +0300

    add class Animal with magic method

--- Program.py
+++ Program.py
@@ -1,31 +1,17 @@
-import pygame as pg
-pg.init()
-
-def management(running, PERFORMANCE_INPUT, inputString):
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        if event.type == pg.KEYDOWN:
-            inputString+=event.unicode
-            print(event.unicode, inputString)
-    SCREEN.fill(COLORS['RED'])
-    text_cur = font.render(inputString, True, COLORS['WHITE'])
-    pg.draw.rect(SCREEN, COLORS['BLACK'], rect)
-    SCREEN.blit(text_cur, (100, 100))
-    pg.display.update()
-    return running, PERFORMANCE_INPUT, inputString
-
-clock = pg.time.Clock()
-running = True
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
-SCREEN = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-rect = pg.Rect((100, 100, 400, 40))
-PERFORMANCE_INPUT = False
-COLORS = {'WHITE':(255, 255, 255), 'BLACK':(0, 0, 0), 'RED':(255, 0, 0)}
-font = pg.font.Font(None, 100)
-inputString = ''
-
-while running:
-    running, PERFORMANCE_INPUT, inputString = management(running, PERFORMANCE_INPUT, inputString)
-pg.quit()
+class Animal():
+    def __init__(self, typeAnimal, strenght):
+        self.typeAnimal = typeAnimal
+        self.type = type
+        self.strenght = strenght
+    def __add__(self, otherAnimal):
+        print('Привет, ', otherAnimal, ', я - '+self.typeAnimal)
+        print('Давай дружить')
+    def __lt__(self, otherAnimal):
+        if self.strenght<otherAnimal.strenght:
+            print('Я тебя боюсь!')
+        else:
+            print('Не боюсь я тебя!')
+    
+hare = Animal('Заяц', 2)
+wolf = Animal('Волк', 4)
+a = hare<wolf
