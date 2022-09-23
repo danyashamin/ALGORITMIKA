494ae9d349f11e5c1195a7eef4872a90e3b732d8
shamin daniil <danya.shamin.06@mail.ru>
2022-09-23 14:40:50 +0300

    Add class with magic method

--- Program.py
+++ Program.py
@@ -1,17 +1,9 @@
-class Animal():
-    def __init__(self, typeAnimal, strenght):
-        self.typeAnimal = typeAnimal
-        self.strenght = strenght
-    def __add__(self, otherAnimal):
-        print('Привет, ', otherAnimal.typeAnimal, ', я - '+self.typeAnimal)
-        print('Давай дружить')
-    def __lt__(self, otherAnimal):
-        if self.strenght<otherAnimal.strenght:
-            print('Я тебя боюсь!')
-        else:
-            print('Не боюсь я тебя!')
-    
-hare = Animal('Заяц', 2)
-wolf = Animal('Волк', 4)
-a = hare<wolf
-b = hare+wolf
+class Player(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __gt__(self, other):
+        return self.age - other.age
+p1 = Player('Игорь', 12)
+p2 = Player('Анна', 13)
+print(p1 > p2)
