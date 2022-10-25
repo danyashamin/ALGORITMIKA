889c340e8197214a4eeab96233b7eca7b44d6093
shamin daniil <danya.shamin.06@mail.ru>
2022-10-25 16:04:31 +0300

    delete almost all code

--- Program.py
+++ Program.py
@@ -1,11 +1,2 @@
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 import sys
-
-if __name__ == '__main__':
-    app_1 = QApplication(sys.argv)
-    widget_1 = QWidget()
-    widget_1.resize(300, 200)
-    widget_1.move(300, 300)
-    widget_1.setWindowTitle('MyWindow')
-    widget_1.show()
-    sys.exit(app_1.exec_())
