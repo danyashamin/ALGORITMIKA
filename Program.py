4108605f290ea445cf908786c3a259f1baec1203
shamin daniil <danya.shamin.06@mail.ru>
2022-10-26 20:11:43 +0300

    add alimpiade task

--- Program.py
+++ Program.py
@@ -1,2 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-import sys
+first = int(input('Введите длительность первого фильма:'))#длительность первого фильма
+second = int(input('Введите длительность второго фильма:'))#длительность второго фильма
+cur_time = int(input('Введите текущее время:'))#текущее время
+
+if cur_time%first < cur_time%second:
+    print(cur_time%first)
+    print('Иди на первый')
+elif cur_time%first > cur_time%second:
+    print(cur_time%second)
+    print('Иди на второй')
+else:
+    print('Иди куда хочешь')
