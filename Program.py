f490cce9d4f3c671482949d99b2dcd9492bd0225
shamin daniil <danya.shamin.06@mail.ru>
2022-10-29 12:01:27 +0300

    add alimpiade task

--- Program.py
+++ Program.py
@@ -1,12 +1,6 @@
-first = int(input('Введите длительность первого фильма:'))#длительность первого фильма
-second = int(input('Введите длительность второго фильма:'))#длительность второго фильма
-cur_time = int(input('Введите текущее время:'))#текущее время
-
-if cur_time%first < cur_time%second:
-    print(cur_time%first)
-    print('Иди на первый')
-elif cur_time%first > cur_time%second:
-    print(cur_time%second)
-    print('Иди на второй')
-else:
-    print('Иди куда хочешь')
+a = 'Python'
+a_list = list()
+for i in a:
+    a_list.append(i)
+a = a_list
+print(a)
