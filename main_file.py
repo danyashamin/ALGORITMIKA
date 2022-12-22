50382679fabe788b648464469813a60cce2fd4ec
shamin daniil <danya.shamin.06@mail.ru>
2022-12-22 17:09:36 +0300

    do PyQt5 work

--- main_file.py
+++ main_file.py
@@ -1,2 +1,18 @@
-with open('file_java.java', 'r') as f_java:
-    f_java.close()
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+import sys
+
+class StartWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 1000, 500)
+        self.show()
+        print(Qt.AlignLeft)
+        global count_cycle
+        if count_cycle==0:
+            count_cycle+=1
+            sys.exit(app.exec_())
+
+app = QApplication(sys.argv)
+count_cycle = 0
+w = StartWin()
