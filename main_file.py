a7588459330419fd2650590f1a631275a8989e63
shamin daniil <danya.shamin.06@mail.ru>
2022-12-22 17:19:59 +0300

    PyQt5 development

--- main_file.py
+++ main_file.py
@@ -1,18 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
-from PyQt5.QtCore import Qt
-import sys
+import json
 
-class StartWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(200, 200, 1000, 500)
-        self.show()
-        print(Qt.AlignLeft)
-        global count_cycle
-        if count_cycle==0:
-            count_cycle+=1
-            sys.exit(app.exec_())
-
-app = QApplication(sys.argv)
-count_cycle = 0
-w = StartWin()
+with open('file_j.json', 'w') as f_cur:
+    need_dict = {1:"1", 2:"2"}
+    json.dump(need_dict, f_cur)
+    f_cur.close()
