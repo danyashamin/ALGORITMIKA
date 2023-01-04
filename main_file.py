8d0eaee3f7be139cc0268853c8549e49a7e7774c
shamin daniil <danya.shamin.06@mail.ru>
2023-01-04 16:00:40 +0300

    add main window

--- main_file.py
+++ main_file.py
@@ -1,13 +1,61 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from PIL import Image, ImageFilter
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 import sys
 
-class NeedWin(QMainWindow):
-    def __init__(self):
+class NeedWindow(QMainWindow):
+    def __init__(self, *args):
+        #def common_fun():
         super().__init__()
-        self.setGeometry(300, 300, 1000, 500)
+        self.setWindowTitle('Моё окно')
+        self.setGeometry(200, 100, 1000, 600)
+        self.win_main = QWidget()
+        self.main_line = QVBoxLayout()
+        self.HLine = QHBoxLayout()
+        self.HLine.setSpacing(2)
+        if len(args)>0:
+            with Image.open(args[0]) as image_cur:
+                image_cur = image_cur.resize((500, 500))
+                image_cur.save(args[0][0:9]+'.small.jpg')
+            self.pix_cur = QPixmap(args[0][0:9]+'.small.jpg')
+            self.label_image = QLabel(self)
+            self.label_image.setPixmap(self.pix_cur)
+            self.HLine.addWidget(self.label_image, stretch=1)
+        else:
+            self.HLine.addStretch(1)
+            self.VLine = QVBoxLayout()
+            self.VLine.setSpacing(8)
+            self.input_label = QLabel(self)
+            self.input_label.setText('Введите имя изображения                                                   ')
+            self.VLine.addWidget(self.input_label)
+            self.input_line = QLineEdit()
+            self.input_line.setPlaceholderText('Введите место сущестующего изображения')
+            self.VLine.addWidget(self.input_line)
+            self.input_button = QPushButton('Ввёл')
+            self.input_button.clicked.connect(self.input_reaction)
+            self.VLine.addWidget(self.input_button)
+            self.VLine.addStretch(5)
+            self.HLine.addLayout(self.VLine)
+
+        self.main_line.addLayout(self.HLine)
+        self.win_main.setLayout(self.main_line)
         
+        self.setCentralWidget(self.win_main)
         self.show()
-        sys.exit(app.exec_())
+        global count_cycle
+        if count_cycle==0:
+            count_cycle+=1
+            sys.exit(app.exec_())
+    def input_reaction(self):
+        try:
+            self.close()
+            self.__init__(self.input_line.text())
+        except:
+            pass
+
+#global constants
+count_cycle = 0
+
 app = QApplication(sys.argv)
-w = NeedWin()
+n = NeedWindow()
