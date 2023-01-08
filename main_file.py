745fb9c95536aa06a7408ad24c93a4e78f65d6f0
shamin daniil <danya.shamin.06@mail.ru>
2023-01-08 16:57:30 +0300

    do algoritmika lessen

--- main_file.py
+++ main_file.py
@@ -1,27 +1,58 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+from PIL import Image, ImageEnhance
 import os
 import sys
 
 class NeedWindow(QMainWindow):
     def __init__(self, name):
         super().__init__()
+        self.setWindowTitle('Лучшее приложение в мире')
         self.setGeometry(300, 300, 1000, 500)
         self.main_line = QVBoxLayout()
+        self.main_line.setSpacing(3)
         self.common_line = QHBoxLayout()
         self.common_line.setSpacing(2)
         self.label_image = QLabel(self)
         self.pix_image = QPixmap(name)
-        self.label_image.setPixmap(self.pix_image)
-        self.common_line.addWidget(self.label_image)
+        self.pix_on_screen = Image.open(name)
+        self.pix_on_screen = self.pix_on_screen.resize((int(self.pix_on_screen.size[0]/3), int(self.pix_on_screen.size[1]/3)))
+        self.pix_on_screen.save(name[0:len(name)-4]+'screen.jpg')
+        self.pix_on_screen = QPixmap(name[0:len(name)-4]+'screen.jpg')
+        self.label_image.setPixmap(self.pix_on_screen)
+        self.common_line.addWidget(self.label_image, stretch=1)
+        self.buttons_line = QVBoxLayout()
+        self.buttons_line.setSpacing(3)
+        self.cut_or_resize = QHBoxLayout()
+        self.cut_or_resize.setSpacing(3)
+        self.cut_button = QPushButton('Обрезать')
+        self.cut_or_resize.addWidget(self.cut_button, stretch=1)
+        self.cut_or_resize.addStretch(1)
+        self.resize_button = QPushButton('Изменить размер')
+        self.cut_or_resize.addWidget(self.resize_button, stretch=1)
+        self.buttons_line.addLayout(self.cut_or_resize)
+        self.convert_or_contrast = QHBoxLayout()
+        self.convert_or_contrast.setSpacing(3)
+        self.convert_button = QPushButton('Сделать чёрно-белым')
+        self.convert_or_contrast.addWidget(self.convert_button, stretch=1)
+        self.convert_or_contrast.addStretch(1)
+        self.contrast_button = QPushButton('Увеличить контраст')
+        self.convert_or_contrast.addWidget(self.contrast_button, stretch=1)
+        self.buttons_line.addLayout(self.convert_or_contrast)
+        self.save_line = QHBoxLayout()
+        self.save_line.setSpacing(3)
+        self.save_button = QPushButton('Сохранить')
+        self.save_line.addWidget(self.save_button, stretch=1)
+        self.buttons_line.addLayout(self.save_line)
+        self.common_line.addLayout(self.buttons_line, stretch=1)
         self.main_line.addLayout(self.common_line)
         self.help_win = QWidget()
         self.help_win.setLayout(self.main_line)
         self.setCentralWidget(self.help_win)
         self.show()
 
-def start_win():
+def image_work_win():
     def start():
         print(os.path.isfile(line_input.text()))
         if os.path.isfile(line_input.text()):
@@ -60,4 +91,4 @@ count_win = 0
 app = QApplication(sys.argv)
 
 
-start_win()
+image_work_win()
