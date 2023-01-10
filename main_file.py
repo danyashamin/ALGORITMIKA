7fd9b6f2cec485d7c66d2890fb3cac96b0a32564
shamin daniil <danya.shamin.06@mail.ru>
2023-01-10 17:25:29 +0300

    add some buttons

--- main_file.py
+++ main_file.py
@@ -1,37 +1,78 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QTextEdit, QLineEdit, QPushButton
+from PyQt5.QtGui import QPixmap
+from PIL import Image, ImageFilter
 import sys
+import os
 
-class MyWind(QWidget):
-    def __init__(self):
+class NeedWindow(QMainWindow):
+    def __init__(self, *args):
         super().__init__()
-        self.win_2 = QWidget()
-        self.win_2.setGeometry(100, 100, 100, 100)
-        self.setGeometry(300, 300, 500, 500)
-        self.main_line = QVBoxLayout()
-        self.button_line = QHBoxLayout()
-        self.button_line.setSpacing(5)
-        self.button_line.addStretch(2)
-        self.button_1 = QPushButton('Кнопка_1')
-        self.button_1.clicked.connect(self.func_1)
-        self.button_line.addWidget(self.button_1, stretch=1)
-        self.button_line.addStretch(2)
-        self.main_line.addLayout(self.button_line)
-        self.setLayout(self.main_line)
-
-        self.show()
-        global count_app
-        print(count_app)
-        if count_app==0:
-            count_app+=1
-            sys.exit(app.exec_())
+        self.setWindowTitle('ImageMaker')
+        win = QWidget()
+        global count_NeedWindow
+        if count_NeedWindow==0:
+            main_line = QVBoxLayout()
+            common_line = QHBoxLayout()
+            common_line.setSpacing(2)
+            common_line.addStretch(1)
+            line_small_widgets = QVBoxLayout()
+            line_small_widgets.setSpacing(7)
+            input_label = QLabel()
+            input_label.setText('Введите имя фотографии:')
+            line_small_widgets.addWidget(input_label, stretch=1)
+            line_input = QLineEdit()
+            line_small_widgets.addWidget(line_input, stretch=1)
+            button_input = QPushButton('Искать изображение по названию')
+            def input_reaction():
+                if os.path.isfile(line_input.text()):
+                    text_on_line = line_input.text()
+                    global count_NeedWindow
+                    count_NeedWindow+=1
+                    win.hide()
+                    self.hide()
+                    self.__init__(text_on_line)
+            button_input.clicked.connect(input_reaction)
+            line_small_widgets.addWidget(button_input, stretch=1)
+            line_small_widgets.addStretch(4)
+            common_line.addLayout(line_small_widgets, stretch=1)
+            main_line.addLayout(common_line)
         else:
-            self.win_2.show()
-    def func_1(self):
-        global count_app
-        self.hide()
-        count_app+=1
-        self.__init__()
-
+            main_line = QVBoxLayout()
+            common_line = QHBoxLayout()
+            common_line.setSpacing(2)
+            with Image.open(args[0]) as f_image:
+                f_image = f_image.resize((int(f_image.size[0]/3), int(f_image.size[1]/3)))
+                f_image.save(args[0][0:len(args[0])-4]+'.small.jpg')
+            pix_small = QPixmap(args[0][0:len(args[0])-4]+'.small.jpg')
+            pix_label = QLabel(self)
+            pix_label.setPixmap(pix_small)
+            common_line.addWidget(pix_label, stretch=1)
+            line_for_buttons = QVBoxLayout()
+            cut_or_resize = QHBoxLayout()
+            cut_or_resize.setSpacing(2)
+            cut = QPushButton('Обрезать')
+            cut_or_resize.addWidget(cut, stretch=1|2)
+            resize = QPushButton('Изменить размер')
+            cut_or_resize.addWidget(resize, stretch=1|2)
+            line_for_buttons.addLayout(cut_or_resize)
+            convert_or_contrast = QHBoxLayout()
+            convert_or_contrast.setSpacing(2)
+            convert = QPushButton('Сделать чёрно белым')
+            convert_or_contrast.addWidget(convert, stretch=1|2)
+            contrast = QPushButton('Сделать контрастным')
+            convert_or_contrast.addWidget(contrast, stretch=1|2)
+            line_for_buttons.addLayout(convert_or_contrast)
+            common_line.addLayout(line_for_buttons, stretch=1)
+            main_line.addLayout(common_line)
+        win.setLayout(main_line)
+        self.setCentralWidget(win)
+        self.show()
+        global count_App
+        if count_App==0:
+            count_App+=1
+            sys.exit(app.exec_())
 app = QApplication(sys.argv)
-count_app = 0
-m = MyWind()
+count_NeedWindow = 0
+count_App = 0
+
+n = NeedWindow()
